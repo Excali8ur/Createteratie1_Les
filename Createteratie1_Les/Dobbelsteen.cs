ebc6af29abe4059c519a3c075830d1108a8b0398
2404835 <2404835janssen@zuyd.nl>
2025-05-20 11:42:31 +0200

    class dobbelsteen

---
+++ Createteratie1_Les/Dobbelsteen.cs
@@ -0,0 +1,16 @@
+﻿namespace Createteratie1_Les
+{
+    public class Dobbelsteen
+    {
+        public string Kleur { get; set; }
+        public string KleurOgen { get; set; }
+        public string Vorm { get; set; }
+        public string Materiaal { get; set; }
+        public int AantalZijden { get; set; }
+        public List<int> WaardePerZijden{ get; set; }
+        public int Maxwaarden { get; set; }
+        public int Minwaarden { get; set; }
+        public string Grote { get; set; }
+
+    }
+}
--- Createteratie1_Les/Program.cs
+++ Createteratie1_Les/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            if (x > 5)
-            {      
-                Console.WriteLine("bryan was here");
-            }
+            Dobbelsteen = new Dobbelsteen();
+            Dobbelsteen.Kleur = "groen";
+            Dobbelsteen.KleurOgen = "zwart";
+            Dobbelsteen.Vorm = "kubus";
+            Dobbelsteen.AantalZijden = 6;
+            Dobbelsteen.WaardePerZijden = new List<int>([1, 2, 3, 4, 5, 6]);
+            Dobbelsteen.Maxwaarde = 6;
+            Dobbelsteen.Minwaarden = 1;
+            Dobbelsteen.Grote = "groot";
         }
     }
 }
