e3610b981aa4ecf0f25975e3dd6224aaaba777ba
Rianne Boumans <rianne.boumans@zuyd.nl>
2025-05-20 10:13:25 +0200

    Add project files.

---
+++ Createteratie1_Les/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Createteratie1_Les
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            if (x > 5)
+            {
+                Console.WriteLine("x > 5");
+            }
+
+        }
+    }
+}
