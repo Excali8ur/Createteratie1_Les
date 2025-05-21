642f1acaadd47fdef227569ae0f05fe499e397a7
Rianne Boumans <rianne.boumans@zuyd.nl>
2025-05-21 10:59:03 +0200

    Klasse Dobbelsteen afgemaakt, inclusief commentaar. En voorbeelden voor het gebruik toegevoegd aan de Main.

--- Createteratie1_Les/Dobbelsteen.cs
+++ Createteratie1_Les/Dobbelsteen.cs
@@ -8,15 +8,125 @@ namespace Createteratie1_Les
 {
     public class Dobbelsteen
     {
+        //Attributen / Eigenschappen / Properties
         public string Kleur { get; set; }
         public string KleurOgen { get; set; }
         public string Vorm { get; set; }
         public string Materiaal { get; set; }
+        public string Grote { get; set; }
+        
         public int AantalZijden { get; set; }
-        public List<int> WaardePerZijde { get; set; }
         public int MaxWaarde {  get; set; }
         public int MinWaarde { get; set; }
-        public string Grote {  get; set; }
+        //public List<int> WaardePerZijde { get; set; }
+        public List<string> WaardePerZijde { get; set; }
+
+        //Deze variabele wordt gebruikt om de dobbelsteen te kunnen Rollen.
+        //Private omdat de buitenwereld deze niet hoeft te kennen.
+        //(Encapsulation)
+        private Random willekeurigeWaarde;
+
+        /// <summary>
+        /// Standaard constructor waarmee een normale groene 
+        /// dobbelsteen met 6 zijden aangemaakt wordt
+        /// </summary>
+        public Dobbelsteen()
+        {
+            Kleur = "Groen";
+            KleurOgen = "Zwart";
+            Vorm = "Kubus";
+            Materiaal = "Plastic";
+            Grote = "Normaal";
+
+            AantalZijden = 6;            
+            MaxWaarde = 6;
+            MinWaarde = 1;
+            //WaardePerZijde = new List<int>([1, 2, 3, 4, 5, 6]);
+            WaardePerZijde = new List<string>();
+            MaakWaardePerZijden();
+
+            willekeurigeWaarde = new Random();
+        }
+
+        /// <summary>
+        /// Constructor van een Dobbelsteen waarbij alle gegevens meegegeven
+        /// worden als parameters.
+        /// </summary>
+        /// <param name="kleur">Kleur van de dobbelsteen</param>
+        /// <param name="kleurOgen">Kleur van de waarden op de dobbelsteen</param>
+        /// <param name="vorm">Vorm van de dobbelsteen</param>
+        /// <param name="materiaal">Materiaal van de dobbelsteen</param>
+        /// <param name="grote">Formaat van de dobbelsteen (Mini, Klein, Normaal, Groot, Extra Groot)</param>
+        /// <param name="aantalZijden">Hoeveel vlakken heeft de dobbelsteen</param>
+        /// <param name="waardePerZijde">Wat staat er op iedere zijde?</param>
+        /// <param name="maxWaarde">Maximale waarde, indien de dobbelsteen cijfers bevat</param>
+        /// <param name="minWaarde">Minimale waarde, indien de dobbelsteen cijfers bevat</param>
+        public Dobbelsteen(string kleur, string kleurOgen, 
+            string vorm, string materiaal, string grote,
+            int aantalZijden, int maxWaarde, int minWaarde,
+            List<string> waardePerZijde)
+        {
+            Kleur = kleur;
+            KleurOgen = kleurOgen;
+            Vorm = vorm;
+            Materiaal = materiaal;
+            Grote = grote;
+
+            AantalZijden = aantalZijden;
+            WaardePerZijde = waardePerZijde;
+            MaxWaarde = maxWaarde;
+            MinWaarde = minWaarde;
+
+            willekeurigeWaarde = new Random();
+        }
+
+        //Hulpfunctie die de lijst "WaardePerZijde" opbouwt
+        //op basis van de variabelen MinWaarde en MaxWaarde.
+        //Deze is alleen beschikbaar binnen de klasse.
+        //Als er geen parameter meegegeven wordt,
+        //wordt automatisch stapgrote 1 gebruikt
+        private void MaakWaardePerZijden(int stapgrote = 1)
+        {
+            WaardePerZijde = new List<string>();
+            int waarde = MinWaarde;
+            while (waarde <= MaxWaarde)
+            {
+                WaardePerZijde.Add(waarde.ToString());
+                waarde+= stapgrote;
+            }
+        }
+
+        //Methoden / Gedrag van een dobbelsteen
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>De gerolde waarde als string</returns>
+        public string Rollen()
+        {
+            //Bepaal een willekeurig getal tussen 0 en de lengte van de lijst (= aantalZijden)
+            int geroldeWaarde = willekeurigeWaarde.Next(AantalZijden);
+            
+            //Geef de waarde terug die op deze index in de lijst staat.
+            return WaardePerZijde[geroldeWaarde];
+        }
+
+        public string ToonGegevens()
+        {
+            string tekst = "Dit is een " + Kleur + " dobbelsteen met " + KleurOgen + " waarden en heeft " + AantalZijden;
+            tekst += "\nHij is gemaakt van " + Materiaal + " en heeft het formaat " + Grote +".";
+            return tekst;
+        }
+
+        public string ToonWaarden()
+        {
+            string tekst = "De volgende waarden kunnen gerold worden:";
+            foreach (string waarde in WaardePerZijde)
+            {
+                tekst += "\n* " + waarde;
+            }
+            return tekst;
+        }
     }
 }
 
--- Createteratie1_Les/Program.cs
+++ Createteratie1_Les/Program.cs
@@ -2,24 +2,109 @@
 {
     internal class Program
     {
-        static void MaakDobbelstenen()
+        static List<Dobbelsteen> MaakDobbelstenen()
         {
-            Dobbelsteen d1 = new Dobbelsteen();
-            d1.Kleur = "Groen";
-            d1.KleurOgen = "Zwart";
-            d1.Vorm = "Kubus";
-            d1.Materiaal = "Foam";
-            d1.AantalZijden = 6;
-            //d1.WaardePerZijde = "1 t/m 6";
-            d1.WaardePerZijde = new List<int>([1, 2, 3, 4, 5, 6]);
-            d1.MaxWaarde = 6;
-            d1.MinWaarde = 1;
-            d1.Grote = "Groot";
+            List<Dobbelsteen> dobbelstenenLijst = new List<Dobbelsteen>();
+
+            //Verschillende objecten van het type dobbelsteen:
+
+            //Hier wordt de standaard constructor aangeroepen
+            //Zonder parameters, dus je krijgt een object met standaardwaarden
+            Dobbelsteen groeneD6 = new Dobbelsteen();
+
+            Dobbelsteen rodeD6 = new Dobbelsteen();
+            //Aanpassing van de dobbelsteen -> Rood ipv de standaard Groen,
+            //Rest is gelijk
+            rodeD6.Kleur = "Rood";
+
+            List<string> waardenBlauw = new List<string>();
+            int waarde = 1;
+            while (waarde <= 6)
+            {
+                waardenBlauw.Add(waarde.ToString());
+                waarde++;
+            }
+
+            //Hier wordt de tweede constuctor aangeroepen,
+            //waarbij alle gegevens meegegeven worden als parameters
+            //Ook al zijn onderdelen hetzelfde als de standaard,
+            //moeten deze alsnog meegegeven worden.
+            Dobbelsteen groteBlauweD6 = new Dobbelsteen(
+                "Blauw", "Zwart", "Kubus", "Foam", "Groot", 
+                6, 6, 1, waardenBlauw);
+
+            List<string> klusjes = new List<string>();
+            klusjes.Add("Koken");
+            klusjes.Add("Afwassen");
+            klusjes.Add("Onkruid wieden");
+            klusjes.Add("Koffie zetten");
+            klusjes.Add("Ramen lappen");
+            klusjes.Add("Niks doen :)");
+
+            //Dobbelsteen met tekst ipv getallen
+            Dobbelsteen activiteitenDobbelsteen = new Dobbelsteen(
+                "Zilver", "Meerkleurig", "Kubus", "Metaal", "Normaal",
+                6, 0, 0, klusjes);
+
+
+            //Paarse dobbelsteen met gele ogen.
+            Dobbelsteen paarseD100 = new Dobbelsteen();
+            paarseD100.AantalZijden = 10;
+
+            List<string> waardePerZijde = new List<string>();
+            waarde = 0;
+            while (waarde <= 90)
+            {
+                waardePerZijde.Add(waarde.ToString());
+                waarde += 10;
+            }
+            //Welke waarden heeft deze dobbelsteen?
+            paarseD100.WaardePerZijde = waardePerZijde;
+
+            //Oefening: Hoe ziet deze dobbelsteen uit?
+            //Welke waarden moeten nog aangepast worden tov de standaard dobbelsteen?
+
+            dobbelstenenLijst.Add(groeneD6);
+            dobbelstenenLijst.Add(rodeD6);
+            dobbelstenenLijst.Add(groteBlauweD6);
+            dobbelstenenLijst.Add(activiteitenDobbelsteen);
+            dobbelstenenLijst.Add(paarseD100);
+            return dobbelstenenLijst;
+        }
+
+        static void ToonDobbelstenen(List<Dobbelsteen> dobbelstenenLijst)
+        {
+            foreach (Dobbelsteen d in dobbelstenenLijst)
+            {
+                Console.WriteLine(new string('-', 50));
+                Console.WriteLine(d.ToonGegevens());
+                Console.WriteLine(d.ToonWaarden());
+            }
         }
 
         static void Main(string[] args)
         {
-            MaakDobbelstenen();
+            List<Dobbelsteen> dobbelstenenLijst;
+
+            dobbelstenenLijst = MaakDobbelstenen();
+            ToonDobbelstenen(dobbelstenenLijst);
+
+            //Rol alle dobbelstenen een aantal keren achter elkaar:            
+            int aantalKeerRollen = 10;
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Rol alle dobbelstenen "+aantalKeerRollen+"x:");
+            
+            for (int i = 1; i <= aantalKeerRollen; i++)
+            {
+                Console.WriteLine(new string('~', 25));
+                Console.WriteLine("Rol " + i.ToString());
+                Console.WriteLine(new string('~', 25));
+
+                foreach (Dobbelsteen d in dobbelstenenLijst)
+                {
+                    Console.WriteLine(d.Kleur + ": " + d.Rollen());
+                }
+            }
         }
     }
 }
